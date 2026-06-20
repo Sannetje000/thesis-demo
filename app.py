e69d5a83a18b519bef4adabcdd02e121b7ab15a4
Sannetje000 <sannepuister@gmail.com>
2026-06-21 00:56:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -66,14 +66,8 @@ def classify(sentences, tokenizer, model):
 
 st.markdown("<h3 style='text-align: center; margin-bottom: -1rem;'>Dutch Blurb Sentence Classifier</h3>", unsafe_allow_html=True)
 st.markdown("<h5 style='text-align: center; font-weight: normal; font-style: italic;'>Enter a Dutch sentence that could appear in a book blurb.</h4>", unsafe_allow_html=True)
-st.markdown("""
-<p style='color:#000000; font-size:0.95em'>
-<span style='color:#6AAAD4; font-size:1.2em'>●</span> <b style='color:#6AAAD4'>Content</b>: sentences describing the story, characters, or setting<br>
-<span style='color:#E08070; font-size:1.2em'>●</span> <b style='color:#E08070'>Non-content</b>: promotional text, author credits, critic quotes, or other metadata
-</p>
-""", unsafe_allow_html=True)
 
-st.markdown("**######Try an example:**")
+st.markdown("######**Try an example:**")
 cols = st.columns(len(EXAMPLES))
 for i, ex in enumerate(EXAMPLES):
     with cols[i]:
@@ -82,11 +76,20 @@ for i, ex in enumerate(EXAMPLES):
         st.caption(ex["en"])
 
 sentence = st.text_area(
-    "**######Or enter your own Dutch sentence:**",
+    "######**Or enter your own Dutch sentence:**",
     value=st.session_state.get("input_sentence", ""),
     height=100,
     key="input_sentence"
 )
+
+st.markdown("""
+<p style='color:#000000; font-size:0.95em'>
+<span style='color:#6AAAD4; font-size:1.2em'>●</span> <b style='color:#6AAAD4'>Content</b>: sentences describing the story, characters, or setting<br>
+<span style='color:#E08070; font-size:1.2em'>●</span> <b style='color:#E08070'>Non-content</b>: promotional text, author credits, critic quotes, or other metadata
+</p>
+""", unsafe_allow_html=True)
+
+if st.button("Classify"):
 
 if st.button("Classify"):
     if not sentence.strip():
