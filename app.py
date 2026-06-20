b5fbfe3c0fa373f6308ac5f36c28fbec7cf9b6d9
Sannetje000 <sannepuister@gmail.com>
2026-06-21 01:01:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -67,12 +67,6 @@ def classify(sentences, tokenizer, model):
 st.markdown("<h3 style='text-align: center; margin-bottom: -1rem;'>Dutch Blurb Sentence Classifier</h3>", unsafe_allow_html=True)
 st.markdown("<h5 style='text-align: center; font-weight: normal; font-style: italic;'>Enter a Dutch sentence that could appear in a book blurb.</h5>", unsafe_allow_html=True)
 
-st.markdown("""
-<p style='color:#000000; font-size:0.95em'>
-<span style='color:#6AAAD4; font-size:1.2em'>●</span> <b style='color:#6AAAD4'>Content</b>: sentences describing the story, characters, or setting<br>
-<span style='color:#E08070; font-size:1.2em'>●</span> <b style='color:#E08070'>Non-content</b>: promotional text, author credits, critic quotes, or other metadata
-</p>
-""", unsafe_allow_html=True)
 
 st.markdown("<p style='font-weight:bold;'>Try an example:</p>", unsafe_allow_html=True)
 cols = st.columns(len(EXAMPLES))
@@ -83,6 +77,14 @@ for i, ex in enumerate(EXAMPLES):
         st.caption(ex["en"])
 
 st.markdown("<p style='font-weight:bold;'>Or enter your own Dutch sentence:</p>", unsafe_allow_html=True)
+
+st.markdown("""
+<p style='color:#000000; font-size:0.95em'>
+<span style='color:#6AAAD4; font-size:1.2em'>●</span> <b style='color:#6AAAD4'>Content</b>: sentences describing the story, characters, or setting<br>
+<span style='color:#E08070; font-size:1.2em'>●</span> <b style='color:#E08070'>Non-content</b>: promotional text, author credits, critic quotes, or other metadata
+</p>
+""", unsafe_allow_html=True)
+
 sentence = st.text_area(
     "",
     value=st.session_state.get("input_sentence", ""),
