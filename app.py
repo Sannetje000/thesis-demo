82b4e1afac5a11aed8c7705ad7d6a5fab2d446f9
Sannetje000 <sannepuister@gmail.com>
2026-06-21 01:31:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ for i, ex in enumerate(EXAMPLES):
 st.markdown("<p style='font-weight:bold;margin-bottom:+2px;margin-top: +1rem;font-size:1.3em;'>Or enter your own Dutch sentence:</p>", unsafe_allow_html=True)
 
 st.markdown("""
-<p style='color:#000000; font-size:0.95em'>
+<p style='color:#000000; font-size:1.1em'>
 <span style='color:#6AAAD4; font-size:1em'>●</span> <b style='color:#6AAAD4'>Content</b>: sentences describing the story, characters, or setting<br>
 <span style='color:#E08070; font-size:1em'>●</span> <b style='color:#E08070'>Non-content</b>: promotional text, author credits, critic quotes, or other metadata
 </p>
